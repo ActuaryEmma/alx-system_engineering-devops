bcbb448f07e5f00030fcef274ab704cdf792f6de
ActuaryEmma <emmanyakio@gmail.com>
2023-10-24 01:26:54 +0300

    formating line

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,24 +7,25 @@ import sys
 import urllib.request
 
 
-if len(sys.argv) != 2:
-    print(f"Usage: {sys.argv[0]} <id>")
-    sys.exit(1)
-if not sys.argv[1].isdigit():
-    print(f"Error: {sys.argv[1]} should be an integer")
-    sys.exit(1)
-url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
-url2 = f"https://jsonplaceholder.typicode.com/todos?userId={sys.argv[1]}"
-with urllib.request.urlopen(url) as f:
-    data = json.loads(f.read().decode('utf-8'))
-with urllib.request.urlopen(url2) as f:
-    data2 = json.loads(f.read().decode('utf-8'))
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <id>")
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print(f"Error: {sys.argv[1]} should be an integer")
+        sys.exit(1)
+    url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
+    url2 = f"https://jsonplaceholder.typicode.com/todos?userId={sys.argv[1]}"
+    with urllib.request.urlopen(url) as f:
+        data = json.loads(f.read().decode('utf-8'))
+    with urllib.request.urlopen(url2) as f:
+        data2 = json.loads(f.read().decode('utf-8'))
 
-# completed = [],, loop through t in data2 check if completed is True,
-# it is append title to completed list
-completed = [t['title'] for t in data2 if t['completed']]
-print("Employee {} is done with tasks({}/{}):".format(
-        data["username"], len(completed), len(data2)))
-# return title list of completed tasks
-for c in completed:
-    print("\t{}".format(c))
+    # completed = [],, loop through t in data2 check if completed is True,
+    # it is append title to completed list
+    completed = [t['title'] for t in data2 if t['completed']]
+    print("Employee {} is done with tasks({}/{}):".format(
+            data["username"], len(completed), len(data2)))
+    # return title list of completed tasks
+    for c in completed:
+        print("\t{}".format(c))
